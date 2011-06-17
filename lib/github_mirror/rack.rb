58529a148a0e139842c1d11630ea9fa1c33a361e
Yann Lugrin <yann.lugrin@liquid-concept.ch>
2011-06-17 22:38:02 +0200

    handle basic request

--- lib/github_mirror/rack.rb
+++ lib/github_mirror/rack.rb
@@ -1,8 +1,35 @@
 # encoding: utf-8
+require 'json'
 
 class GithubMirror
   class Rack
     def call(env)
+      @request  = ::Rack::Request.new(env)
+      @response = ::Rack::Response.new
+
+      handle_request
+
+      @response.finish
+    end
+
+    private
+
+    def handle_request
+      raise(ArgumentError, 'only POST request allowed') unless @request.post?
+      raise(ArgumentError, 'token not match')           if config.token && !@request.path_info.end_with?('/' + config.token)
+
+      payload = JSON.parse(@request[:payload])    rescue raise(ArgumentError, 'payload is not a valid JSON string')
+      mirror  = Mirror.load_from_payload(payload) rescue raise(ArgumentError, 'payload is not a valid Github push')
+
+      mirror.update
+
+      @response.write 'done'
+    rescue Exception => e
+      @response.write "fail: #{e.message}"
+    end
+
+    def config
+      GithubMirror::Config.config
     end
   end
 end
--- spec/lib/github_mirror/rack_spec.rb
+++ spec/lib/github_mirror/rack_spec.rb
@@ -3,7 +3,18 @@ require 'github_mirror/rack'
 require 'spec_helper'
 
 describe GithubMirror::Rack do
+  include ::Rack::Test::Methods
+
   subject { GithubMirror::Rack.new }
+
+  let(:mock_request) { mock('request', :finish => [200, {}, []], :write => nil) }
+  let(:mock_mirror ) { mock('mirror',  :update => true) }
+
+  let(:app) { subject }
+
+  before(:each) do
+    GithubMirror::Mirror.stub!(:load_from_payload).and_return(mock_mirror)
+  end
 
   context '#call' do
 
@@ -11,6 +22,116 @@ describe GithubMirror::Rack do
       lambda { subject.call }.should raise_error(ArgumentError, /wrong number of arguments \(0 for 1\)/)
     end
 
+    it 'should instanciate request' do
+      ::Rack::Request.should_receive(:new).with({})
+      subject.call({})
+    end
+
+    it 'should instanciate response' do
+      ::Rack::Response.should_receive(:new).with(no_args).and_return(mock_request)
+      subject.call({})
+    end
+
+  end
+
+  it 'should reply with fail message on GET' do
+    get '/'
+
+    last_response.should be_ok
+    last_response.body.should =~ /fail:/
+  end
+
+  it 'should reply with fail message on POST without payload' do
+    post '/'
+
+    last_response.should be_ok
+    last_response.body.should =~ /fail:/
+  end
+
+  it 'should reply with fail message on POST with empty payload' do
+    post '/', :payload => ''
+
+    last_response.should be_ok
+    last_response.body.should =~ /fail:/
+  end
+
+  it 'should reply with fail message on POST with invalid payload' do
+    GithubMirror::Mirror.stub!(:load_from_payload).and_return { raise(ArgumentError) }
+
+    post '/', :payload => INVALID_JSON.to_json
+
+    last_response.should be_ok
+    last_response.body.should =~ /fail:/
+  end
+
+  it 'should reply with success message on POST with valid payload' do
+    post '/', :payload => GITHUB_JSON.to_json
+
+    last_response.should be_ok
+    last_response.body.should == 'done'
+  end
+
+  it 'should reply with success message on POST if token match' do
+    app.stub!(:config).and_return(mock('config', :token => 'TOKEN'))
+
+    post '/TOKEN', :payload => GITHUB_JSON.to_json
+
+    last_response.should be_ok
+    last_response.body.should == 'done'
+  end
+
+  it 'should reply with fail message on POST if token not match' do
+    app.stub!(:config).and_return(mock('config', :token => 'TOKEN'))
+
+    post '/FAILTOKEN', :payload => GITHUB_JSON.to_json
+
+    last_response.should be_ok
+    last_response.body.should =~ /fail:/
+  end
+
+  it 'should reply with success message on POST if no token is configured' do
+    app.stub!(:config).and_return(mock('config', :token => nil))
+
+    post '/TOKEN', :payload => GITHUB_JSON.to_json
+
+    last_response.should be_ok
+    last_response.body.should == 'done'
+  end
+
+  pending 'should reply with fail message if owner is not allowed to be mirrored' do
+    @app.stub!(:config).and_return(config({'repositories' => {"#{repository_owner}/*" => {'allowed' => false}}}))
+
+    post '/', :payload => GITHUB_JSON.to_json
+
+    last_response.should be_ok
+    last_response.body.should =~ /fail:/
+  end
+
+  pending 'should reply with succes message if all owner\'s repositories is allowed to be mirrored' do
+    @app.stub!(:config).and_return(config({'repositories' => {"#{repository_owner}/*" => {'allowed' => true}}}))
+
+    post '/', :payload => GITHUB_JSON.to_json
+
+    last_response.should be_ok
+    last_response.body.should == 'done'
+  end
+
+  pending 'should reply with fail message if repository is not allowed to be mirrored' do
+    @app.stub!(:config).and_return(config({'repositories' => {"#{repository_owner}/#{repository_name}" => {'allowed' => false}}}))
+
+    post '/', :payload => GITHUB_JSON.to_json
+
+    last_response.should be_ok
+    last_response.body.should =~ /fail:/
+  end
+
+  pending 'should reply with success message if repository is allowed to be mirrored' do
+    @app.stub!(:config).and_return(config({'repositories' => {"#{repository_owner}/#{repository_name}" => {'allowed' => true}}}))
+
+    post '/', :payload => GITHUB_JSON.to_json
+
+    last_response.should be_ok
+    last_response.body.should == 'done'
   end
 
 end
